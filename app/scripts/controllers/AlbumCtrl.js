2582c1344ba0c55a868d70bcc52a7ca9c906f33b
summerlove91 <gkdus3040@gmail.com>
2017-05-11 16:34:36 +0900

    angular-checkpoint-7

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,12 +1,13 @@
  (function() {
-     function AlbumCtrl(Fixtures) {
+     function AlbumCtrl(Fixtures, SongPlayer) {
      this.albums = [];
      for (var i=0; i < 12; i++){
          this.albumData = Fixtures.getAlbum();
+         this.songPlayer = SongPlayer;
         }
      }
  
      angular
          .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+         .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
  })();
